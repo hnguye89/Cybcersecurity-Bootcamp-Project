94464046018d5522758efb69ce42335fc9749b19
Helen Nguyen <77870466+hnguye89@users.noreply.github.com>
2021-06-16 20:31:35 -0400

    Deleted files

--- Linux/Shell_Scripts/Dealer_Lookup.sh
+++
@@ -1,4 +0,0 @@
-# Dealer_Lookup.sh
- 
-cat Normalised_Dealer_Schedule | grep $1 | grep $2 | grep $3 | awk -F , '{print $1, $2, $3, $5}'
-
--- Linux/Shell_Scripts/roulette_dealer_finder_by_time.sh
+++
@@ -1,1 +0,0 @@
-cat Normalised_Dealer_Schedule | sed 's/:00:00,//g' | grep $1 | grep $2 | awk -F , '{print $4}'
