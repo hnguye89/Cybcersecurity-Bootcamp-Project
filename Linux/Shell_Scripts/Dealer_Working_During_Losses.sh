05459d9f4f1b4bd539f7ac6ffd78fb8ba9443fd4
Helen Nguyen <77870466+hnguye89@users.noreply.github.com>
2021-06-16 20:24:08 -0400

    Added in the files

--- Linux/Shell_Scripts/Dealer_Working_During_Losses.sh
+++ Linux/Shell_Scripts/Dealer_Working_During_Losses.sh
@@ -1,9 +1,3 @@
-# Dealer_Working_During_Losses.sh
-# Author: Luke Vassallo
-# Date: Saturday, 1 August 2020
-
-# SYNOPSIS: ./Dealer_Working_During_Losses.sh
-
 ./Dealer_Lookup.sh 0310 05:00:00 AM
 ./Dealer_Lookup.sh 0310 08:00:00 AM
 ./Dealer_Lookup.sh 0310 02:00:00 PM
